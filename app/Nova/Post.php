badd0559b709ff57c21433f1a1d2b3db2e9720d8
naqeeb-klabs <naqeebsufi.klabs@gmail.com>
2020-10-26 11:55:22 +0500

    nova folder and its resources.

---
+++ app/Nova/Post.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Nova; 
+
+use App\Nova\Actions\PublishPost;
+use App\Nova\Filters\PostCategories;
+use App\Nova\Filters\PostPublished;
+use App\Nova\Lenses\MostTags;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\BelongsToMany;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Select;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Trix;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+use App\Nova\Metrics\PostCount;
+use App\Nova\Metrics\PostsPerDay;
+use App\Nova\Metrics\PostsPerCategory;
+
+class Post extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\Post::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'title';
+
+    public static $globallySearchable = false;
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id', 'title', 'body',
+    ];
+
+    public function title()
+    {
+        return $this->title . ' - ' . $this->category;
+    }
+
+    public function subtitle()
+    {
+        return 'Author: ' . $this->user->name;
+    }
+
+    public static function indexQuery(NovaRequest $request, $query)
+    {
+        return $query->where('user_id', $request->user()->id);
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+
+    public function posts()
+    {
+        $this->hasMany(Post::class);
+    }
+
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+
+            Text::make('Title')->rules('required'),
+
+            Trix::make('body')->rules('required'),
+
+            DateTime::make('Publish At')->hideFromIndex()->rules('after_or_equal:today'),
+
+            DateTime::make('Publish Until')->hideFromIndex()->rules('after_or_equal:publish_at'),
+
+            // Boolean::make('Is Published')
+            //     ->canSee(function ($request) {
+            //         return $request->user()
+            //             ->can('publish_post', $this);
+            //     }),
+
+            Boolean::make('Is Published')
+                ->canSee(function ($request) {
+                    return false;
+                }),
+
+            Select::make('Category')->options([
+                'tutorials' => 'Tutorials',
+                'news' => 'News',
+            ])->hideWhenUpdating()->rules('required'),
+
+            BelongsTo::make('User')->rules('required'),
+
+            BelongsToMany::make('Tags'),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [
+            (new PostsPerDay)->width('full'),
+            (new PostCount)->width('1/2'),         
+            (new PostsPerCategory)->width('1/2'),
+        ];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [
+            new PostPublished,
+            new PostCategories,
+        ];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request)
+    {
+        return [
+            new MostTags,
+        ];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [
+            (new PublishPost)->canSee(function ($request) {
+                return true;
+            })->canRun(function ($request, $post) {
+                return $post->id === 2;
+            }),
+        ];
+    }
+}
